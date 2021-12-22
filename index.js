d382567e99c54fc25c53c9ce1d35a5e728f96711
ghifari-ti <muhammad.ghifari@students.paramadina.ac.id>
2021-12-22 16:23:39 +0700

    fix compiler

---
+++ index.js
@@ -0,0 +1,34 @@
+class Context {
+    contextArray;
+    stateArray;
+    constructor() {
+        this.contextArray = [];
+    }
+    registerArrayContext(arr) {
+        this.contextArray = [...this.contextArray, ...arr];
+    }
+    registerContext(state, callback) {
+        this.contextArray.push({ state: state, callback: callback });
+    }
+    setState(id, state) {
+        let index = this.stateArray.findIndex(state => state.id == id);
+        if (index < 0) {
+            this.stateArray.push({ id: id, state: state });
+            return;
+        }
+        this.stateArray[index].state = state;
+        return;
+    }
+    Context(id) {
+        let found = this.stateArray.find(state => state.id == id);
+        let context = this.contextArray.find(context => context.state == found.state);
+        try {
+            return context.callback();
+        }
+        catch (e) {
+            throw new Error("Context not found");
+        }
+    }
+}
+let context = new Context();
+export default context;
